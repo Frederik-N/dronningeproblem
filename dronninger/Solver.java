f12cca38f5dc723aaaad50478d7bc8d64cdedfd1
jona523c <jonas.madsen2@post.au.dk>
2017-10-24 21:44:35 +0200

    Har fixed alt andet end legal

--- dronninger/Solver.java
+++ dronninger/Solver.java
@@ -8,33 +8,55 @@ public class Solver
     private int noOfQueens;
     private int[] queens;
     private int noOfSolutions;
-    
+
+    /**
+     *
+     * @param noOfQueens
+     */
     public void findAllSolutions(int noOfQueens)
     {
         noOfSolutions = 0;
         this.noOfQueens = noOfQueens;
         queens = new int[noOfQueens+1];
+
+        System.out.println("*-----------------------------------*");
+        System.out.println(" Solutions for "+noOfQueens+" queens:");
+        System.out.println();
+
         long beginTime = System.currentTimeMillis();
         positionQueens(0);
         long endTime = System.currentTimeMillis();
-        System.out.println(noOfSolutions);
-        System.out.println(endTime-beginTime+" ms");
-        
+
+        System.out.println();
+        System.out.println(" A total of "+noOfSolutions+" solutions were found in "+(endTime-beginTime)+" ms");
+        System.out.println("*-----------------------------------*");
     }
-    
+
+    /**
+     *
+     * @param row
+     */
     private void positionQueens(int row) {
+            if (noOfQueens == row) {
+                noOfSolutions += 1;
+                printSolution();
+                System.out.println();
+            }
+
             for(int col = 0; col<noOfQueens; col++) {
-                if(noOfQueens==row) {
-                    noOfSolutions += 1;
-                    return;
-                }
-                else if(legal(row,col)) {
+                if (legal(row, col)) {
                     queens[row] = col;
                     positionQueens(row+1);
                 }
             }
     }
-    
+
+    /**
+     *
+     * @param row
+     * @param col
+     * @return
+     */
     private boolean legal(int row, int col) {
         int k = col+1;
         int p = col-1;
@@ -49,13 +71,29 @@ public class Solver
             p--;
         }
         return true;
-        }
-    
-    private void printSolution() {
-        
     }
-    
+
+    /**
+     *
+     */
+    private void printSolution() {
+        for (int i=0; i<queens.length; i++) {
+            System.out.print(" "+convert(i, queens[i])+" ");
+        }
+    }
+
+    /**
+     *
+     * @param row
+     * @param col
+     * @return
+     */
     private String convert(int row, int col) {
-        return "";
+        return String.valueOf((char)('a'+row))+(col+1);
+    }
+
+    public static void main(String[] args) {
+        Solver test = new Solver();
+        test.findAllSolutions(6);
     }
 }
