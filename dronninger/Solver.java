5abb994c0a60df02a2d86f78d11532e0e4aa5243
jona523c <jonas.madsen2@post.au.dk>
2017-10-24 21:51:26 +0200

    Test af commit

--- dronninger/Solver.java
+++ dronninger/Solver.java
@@ -10,7 +10,7 @@ public class Solver
     private int noOfSolutions;
 
     /**
-     *
+     * Du laver det her shit
      * @param noOfQueens
      */
     public void findAllSolutions(int noOfQueens)
@@ -33,7 +33,7 @@ public class Solver
     }
 
     /**
-     * Du laver det her shit
+     *
      * @param row
      */
     private void positionQueens(int row) {
